700a002c9b964ab4c78a7eb45f47262380bb1e59
OliverHu09 <Hu.Bonino.Oliver@alumnes.fundaciocic.org>
2026-04-22 10:28:20 +0200

    limits

--- limit.gd
+++ limit.gd
@@ -1,7 +1,4 @@
 extends Node2D
-var direccio= Vector2.RIGHT
-var direccio2=Vector2.LEFT
-var velocitat= 100
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,16 +8,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position+= direccio*velocitat*delta
 	pass
 
 
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	queue_free()
+	frontera()
 	pass # Replace with function body.
-func frontera():
-	if(direccio==Vector2.RIGHT):
-		direccio=Vector2.LEFT
-	else:
-		direccio=Vector2.RIGHT
-	global_position += Vector2(0,-50)

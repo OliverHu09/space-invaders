4e175d44fc11e97b663e319357fef3a22bb9459c
OliverHu09 <Hu.Bonino.Oliver@alumnes.fundaciocic.org>
2026-04-23 10:21:46 +0200

    altre alien

---
+++ alien_especial.gd
@@ -0,0 +1,20 @@
+extends Area2D
+var direccio= Vector2.RIGHT
+var velocitat= 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position+= direccio*velocitat*delta
+	pass
+
+func frontera():
+	queue_free()
+
+func _on_area_2d_area_entered(_area: Area2D) -> void:
+	queue_free()
+	pass # Replace with function body.

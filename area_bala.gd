5c7947b8f2e7f55e9d33e97968da82a8b8f6b8fd
SergiE321 <Estape.Perez.Sergi@alumnes.fundaciocic.org>
2026-04-28 08:24:33 +0200

    SENSE ERRORS

--- area_bala.gd
+++ area_bala.gd
@@ -2,8 +2,8 @@ extends Area2D
 
 var speed = 750
 
-func _physics_process(delta):
-	position += speed*Vector2(0, 1)
+func _physics_process(delta: float) -> void:
+	position += speed*Vector2(0, -1)*delta
 
 #func _on_Bullet_body_entered(body):
 	#if body.is_in_group("aliens"):
--- nau__principal.gd
+++ nau__principal.gd
@@ -9,10 +9,14 @@ func _ready() -> void:
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	direccio=Vector2(0, 0)
 	if (Input.is_action_just_pressed("dispar")):
 		shoot()
-	direccio=Input.get_vector("esquerra", "dreta", "blabla", "bleble")
+	if (Input.is_action_just_pressed("dreta")):
+		direccio=Vector2.RIGHT
+	if (Input.is_action_just_pressed("esquerra")):
+		direccio=Vector2.LEFT
+	if (Input.is_action_just_released("esquerra") or Input.is_action_just_released("dreta")):
+		direccio=Vector2(0,0)
 	position+=velocitat*direccio.normalized()*delta
 	move_and_slide()
 func shoot():

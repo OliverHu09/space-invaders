77f7a9e247cb7bdc6cd7c3f131625c49e692134d
SergiE321 <Estape.Perez.Sergi@alumnes.fundaciocic.org>
2026-04-30 10:51:23 +0200

    Enrique

--- area_bala.gd
+++ area_bala.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 var speed = 750
+var viu = true
 
 func _physics_process(delta: float) -> void:
 	position += speed*Vector2(0, -1)*delta
 
 func _on_area_entered(area: Area2D) -> void:
+	var viu = false
 	queue_free()
 	pass # Replace with function body.

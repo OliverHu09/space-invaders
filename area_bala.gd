c9929b3fa9de18be4a681bc98873aad8ca1c153f
SergiE321 <Estape.Perez.Sergi@alumnes.fundaciocic.org>
2026-05-13 10:55:46 +0200

    Bala crashea

--- area_bala.gd
+++ area_bala.gd
@@ -7,12 +7,12 @@ func _ready() -> void:
 	pass
 func _physics_process(delta: float) -> void:
 	position += speed*Vector2(0, -1)*delta
-	
+	if(position.y<0):
+		viu= false
 func _on_area_entered(area: Area2D) -> void:
 	var viu = false
 	queue_free()
 	pass # Replace with function body.
 func _on_body_entered(body):
-	
 	if body.is_in_group("barreres"):
 		queue_free()

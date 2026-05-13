941dc691012b4b61d3f9739adda053e199bd4eaa
OliverHu09 <Hu.Bonino.Oliver@alumnes.fundaciocic.org>
2026-05-13 10:56:34 +0200

    cosas malas

--- bala_alien_node.gd
+++ bala_alien_node.gd
@@ -1,17 +1,15 @@
 extends Area2D
-var speed = 750
-var viu = true
+var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_to_group("projectils")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += speed*Vector2(0, 1)*delta
 	if (position[1]>700):
-			queue_free()
+		queue_free()
 	pass
 
 func _on_body_entered(body: Node2D) -> void:
--- ovni_baix.gd
+++ ovni_baix.gd
@@ -1,22 +1,17 @@
 extends Area2D
 
 @export var bala_alien_node : PackedScene
+@onready var timer: Timer = $Timer
 
 var direccio= Vector2.RIGHT
 var velocitat= 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	timer.wait_time =randf_range(1,10)
+	timer.start()
+
 func _process(delta: float) -> void:
-	#position+= direccio*velocitat*delta
-	var num=randi_range(0,50)
-	if(num==20):
-		var b: Area2D = bala_alien_node.instantiate()
-		%Bales_aliens.add_child(b)
-		b.global_position = global_position
 	pass
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
@@ -25,3 +20,10 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	body.die()
 	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	var b: Area2D = bala_alien_node.instantiate()
+	if is_instance_valid(Global.Bales):
+		Global.Bales.add_child(b)
+	b.global_position = global_position

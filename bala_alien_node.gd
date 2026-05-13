07d698144257cf5499b73b238673a725924ebb72
OliverHu09 <Hu.Bonino.Oliver@alumnes.fundaciocic.org>
2026-05-13 10:37:54 +0200

    cosas

--- bala_alien_node.gd
+++ bala_alien_node.gd
@@ -9,11 +9,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += speed*Vector2(0, -1)*delta
+	position += speed*Vector2(0, 1)*delta
+	if (position[1]>700):
+			queue_free()
 	pass
 
-
-
-
 func _on_body_entered(body: Node2D) -> void:
+	body.die()
+	queue_free()
 	pass # Replace with function body.
--- ovni_baix.gd
+++ ovni_baix.gd
@@ -1,4 +1,7 @@
 extends Area2D
+
+@export var bala_alien_node : PackedScene
+
 var direccio= Vector2.RIGHT
 var velocitat= 100
 
@@ -9,6 +12,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#position+= direccio*velocitat*delta
+	var num=randi_range(0,50)
+	if(num==20):
+		var b: Area2D = bala_alien_node.instantiate()
+		%Bales_aliens.add_child(b)
+		b.global_position = global_position
 	pass
 
 func _on_area_2d_area_entered(area: Area2D) -> void:

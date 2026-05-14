a582e589e89165f7a3314d47bc4625e6784fbf18
OliverHu09 <Hu.Bonino.Oliver@alumnes.fundaciocic.org>
2026-05-14 10:46:02 +0200

    cosas

--- area_bala.gd
+++ area_bala.gd
@@ -5,14 +5,16 @@ var viu = true
 func _ready() -> void:
 	add_to_group("projectils")
 	pass
+	
 func _physics_process(delta: float) -> void:
 	position += speed*Vector2(0, -1)*delta
 	if(position.y<0):
 		viu= false
+		
 func _on_area_entered(area: Area2D) -> void:
 	var viu = false
 	queue_free()
 	pass # Replace with function body.
-func _on_body_entered(body):
-	if body.is_in_group("barreres"):
-		queue_free()
+	
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()

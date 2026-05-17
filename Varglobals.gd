33c317782f7c554451ecada5d258f02ba89a8ff4
SergiE321 <Estape.Perez.Sergi@alumnes.fundaciocic.org>
2026-05-17 17:42:21 +0200

    Para que no pete

--- Varglobals.gd
+++ Varglobals.gd
@@ -2,8 +2,8 @@ extends Node
 @export var area_bala : Area2D
 var primer = false
 var permes = false
-func _process(delta: float) -> void:
-	if(area_bala.postion.y<0):
-		permes = true
-	else:
-		permes = false
+#func _process(delta: float) -> void:
+	#if(area_bala.postion.y<0):
+		#permes = true
+	#else:
+		#permes = false

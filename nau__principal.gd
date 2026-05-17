3d88bca55fe0074c981cc3ef2627f4f8e049df42
SergiE321 <Estape.Perez.Sergi@alumnes.fundaciocic.org>
2026-05-17 17:41:44 +0200

    Cooldown de disparo

--- nau__principal.gd
+++ nau__principal.gd
@@ -3,10 +3,8 @@ extends CharacterBody2D
 @export var Area_bala : PackedScene
 @export var bullet : Area2D
 @onready var player = $Bala
-
-
 @onready var general = $Varglobals
-var primer = general.primer
+@onready var cooldownTimer := $CooldownTimer
 var vides = 3
 var direccio : Vector2=Vector2(0, 0)
 var velocitat: float=300
@@ -15,10 +13,8 @@ func _ready() -> void:
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("dispar")):
-		if(Varglobals.permes==true) or (primer==false):
-			shoot()
-			primer = true
+	if (Input.is_action_just_pressed("dispar") and cooldownTimer.is_stopped()):
+		shoot()
 	direccio.x=Input.get_axis("esquerra", "dreta")
 	position+=velocitat*direccio.normalized()*delta
 	move_and_slide()
@@ -27,7 +23,7 @@ func shoot():
 	var b: Area2D = Area_bala.instantiate()
 	%Bales.add_child(b)
 	b.global_position = global_position
-
+	cooldownTimer.start()
 func die():
 	if (vides==0):
 		queue_free()
